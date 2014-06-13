3821f4e0139ccb0f9d5ae0a40f11175adcefef72
Juan <akkeri@gmail.com>
2014-06-13 02:46:27 +0200

    selfuser.js

---
+++ api/policies/selfuser.js
@@ -0,0 +1,22 @@
+var roleManager = require("../services/rolemanager.js");
+
+module.exports = function(req, res, ok){
+  var error = function(){
+    var requireOwnageError = [{
+      name    : "requireOwnage",
+      message : "You are not allowed",
+      level   : "warning"
+    }];
+    req.session.flash = {err : requireOwnageError};
+    res.redirect("/session/new");
+  };
+  if (!req.session.User) return error();
+  var userId = req.param("userId");
+  if (userId == req.session.User.id){
+    req.self = true;
+    return ok();
+  }
+  roleManager.userAboveUser(req.session.User.id, userId, function(allowed){
+    return allowed ? ok() : error();
+  });
+};
